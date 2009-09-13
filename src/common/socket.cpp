0c946e3ec4a019cec08a09b23a945177de76ce24
Fabrício Godoy <skarllot@gmail.com>
2009-09-13 00:05:02 -0300

    Added subproject library called common.
    Added subproject called common, build as libcommon library. The
    subprojects client and server are already linked to libcommon.

---
+++ src/common/socket.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   socket.cpp
+ * Author: fabricio
+ * 
+ * Created on 12 de Setembro de 2009, 23:43
+ */
+
+#include "socket.h"
+
+cmm::Socket::Socket()
+{
+}
+
+cmm::Socket::~Socket()
+{
+}
+
---
+++ src/common/socket.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   socket.h
+ * Author: fabricio
+ *
+ * Created on 12 de Setembro de 2009, 23:43
+ */
+
+#ifndef _SOCKET_H
+#define	_SOCKET_H
+
+namespace cmm {
+
+    class Socket {
+    public:
+        Socket();
+        virtual ~Socket();
+    private:
+
+    };
+
+}
+#endif	/* _SOCKET_H */
+
