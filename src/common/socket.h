ac9477f0b5d7fe4c83bd316178fe57cbd4a6c27c
Fabrício Godoy <skarllot@gmail.com>
2009-09-13 00:18:14 -0300

    Updated headers from source files.
    Updated headers from source files: added copyright notice, added license
    notice and authors list.

--- src/common/socket.h
+++ src/common/socket.h
@@ -1,8 +1,22 @@
-/* 
- * File:   socket.h
- * Author: fabricio
+/*
+ * Copyright (C) 2009 Fabrício Godoy and Jaelson Wagner
  *
- * Created on 12 de Setembro de 2009, 23:43
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *  Fabrício Godoy <skarllot@gmail.com>
+ *
  */
 
 #ifndef _SOCKET_H
